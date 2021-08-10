a052136da9276720b1c07eb19c8adbfdb59bc1da
skylimdg89 <skylimdg89@gmail.com>
2021-08-10 01:48:25 +0000

    testing

--- server.js
+++ server.js
@@ -2,17 +2,13 @@ const express = require('express');
 const app = express();
 
 app.listen(3000, function(){
-	console.log('listening on 3000...');
-});
-
-app.get('/kingdomofwind', function(req, res){
-	res.send('Welcome to Kingdom of Wind Yeonhwa');
-});
-
-app.get('/calc', function(req, res){
-	res.send('Canculate your exp per hour');
+	console.log('-listening on 3000...');
 });
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/write', function(req, res){
+	res.sendFile(__dirname + '/write.html');
 });
